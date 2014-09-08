99fd82741ff7bc326fee00bc6fb49a41690d7671
Tobias Burger <tobias_burger@hotmail.com>
2014-09-08 16:25:37 +0200

    initial commit

---
+++ fslint/FsLinter.fs
@@ -0,0 +1,89 @@
+﻿module FsLinter
+
+module FsLint =
+
+    open FSharpLint.Framework
+    open Microsoft.FSharp.Compiler.SourceCodeServices
+
+    let loadPlugins () =
+        System.Reflection.Assembly.Load("FSharpLint.Rules")
+        |> LoadVisitors.loadPlugins
+
+    let configCheckers =
+        System.Reflection.Assembly.Load("FSharpLint.Rules")
+        |> LoadVisitors.loadConfigCheckers
+
+    let initVisitorInfo postError =
+        let config = Configuration.loadDefaultConfiguration()
+
+        let configFailures =
+            configCheckers
+            |> (LoadVisitors.checkConfigsForFailures config)
+
+        if configFailures.Length <> 0 then failwith "invalid config"
+
+        let visitorInfo = { Ast.Config = config
+                            Ast.PostError = postError }
+
+        visitorInfo
+
+    let plainTextVisitors (plugins: LoadVisitors.VisitorPlugin list) visitorInfo =
+        [ for plugin in plugins do
+            match plugin.Visitor with
+                | LoadVisitors.Ast(_) -> ()
+                | LoadVisitors.PlainText(visitor) ->
+                    yield visitor visitorInfo ]
+
+    let astVisitors (plugins: LoadVisitors.VisitorPlugin list) visitorInfo =
+        [ for plugin in plugins do
+            match plugin.Visitor with
+                | LoadVisitors.Ast(visitor) ->
+                    yield visitor visitorInfo
+                | LoadVisitors.PlainText(_) -> () ]
+
+    let visitPlainText file input plugins visitorInfo =
+        for visitor in plainTextVisitors plugins visitorInfo do
+            visitor file input
+
+    let visitAst file input plugins visitorInfo = async {
+        let checker = InteractiveChecker.Create()
+
+        let! projectOptions = checker.GetProjectOptionsFromScript("c:/dummy", "")
+        let projectOptions = { projectOptions with ProjectFileNames = [| file |] }
+
+        return
+            astVisitors plugins visitorInfo
+            |> Ast.parse (fun () -> false) checker projectOptions file input
+    }
+
+open FsLint
+open System.IO
+open Microsoft.FSharp.Compiler
+
+let onErrorWithInput f (input: string) =
+    let lines = input.Split('\n')
+    fun (rng: Range.range) msg ->
+        let line = lines.[rng.StartLine-1]
+        f line rng msg
+
+let plugins = lazy loadPlugins()
+
+let asyncFsLintInput onError input = async {
+    let file = "dummy.fs"
+
+    let plugins     = plugins.Force()
+    let visitorInfo = initVisitorInfo (onErrorWithInput onError input)
+
+    do! visitAst       file input plugins visitorInfo
+    do  visitPlainText file input plugins visitorInfo
+}
+
+let asyncFsLintFile onError file = async {
+    let input = File.ReadAllText(file)
+
+    let plugins     = plugins.Force()
+    let visitorInfo = initVisitorInfo (onErrorWithInput onError input)
+
+    do! visitAst       file input plugins visitorInfo
+    do  visitPlainText file input plugins visitorInfo
+}
---
+++ fslint/Program.fs
@@ -0,0 +1,61 @@
+﻿open Nessos.UnionArgParser
+open System
+
+
+type CLIArguments =
+    | [<Mandatory; AltCommandLine("-f")>] Filename of string
+    | [<AltCommandLine("-c")>] Color
+    interface IArgParserTemplate with
+        member s.Usage =
+            match s with
+            | Filename _ -> "specify a file to lint."
+            | Color -> "colorize the console output."
+
+let jsFiddleError next line _ error =
+    printfn " #%i %s" (next()) error
+    printfn "%s" line
+
+let jsFiddleErrorColor next line _ error =
+    printf " #%i " (next())
+    Console.ForegroundColor <- ConsoleColor.DarkYellow
+    printfn "%s" error
+    Console.ForegroundColor <- ConsoleColor.DarkGray
+    printfn "%s" line
+    Console.ResetColor()
+
+let onError line (range : Microsoft.FSharp.Compiler.Range.range) error =
+    let info = sprintf "[%i,%i]" range.StartLine range.StartColumn
+    printfn "%s %s\n> %s" info error line
+
+let onErrorColorized line (range : Microsoft.FSharp.Compiler.Range.range) error =
+    let info = sprintf "[%i,%i]" range.StartLine range.StartColumn
+    printf "%s " info
+    Console.ForegroundColor <- ConsoleColor.Yellow
+    printfn "%s" error
+    Console.ResetColor()
+    printf "> "
+    Console.ForegroundColor <- ConsoleColor.DarkGray
+    printfn "%s" line
+    Console.ResetColor()
+
+[<EntryPoint>]
+let main argv =
+    let parser = UnionArgParser.Create<CLIArguments>()
+    let results = parser.ParseCommandLine(argv, ProcessExiter())
+    let file = results.GetResult <@ Filename @>
+
+    let nrinc =
+        let nr = ref 0
+        fun () ->
+        incr nr
+        !nr
+
+    let onError =
+        if results.Contains <@ Color @> then jsFiddleErrorColor nrinc
+        else jsFiddleError nrinc
+    //    let onError =
+    //        if results.Contains <@ Color @> then onErrorColorized
+    //        else onError
+
+    FsLinter.asyncFsLintFile onError file |> Async.RunSynchronously
+    0
